854e6290fd71ef5710d5d1c6af848fec5846c2ff
Y K <orenoikizama@hotmail.com>
2019-06-09 11:13:30 +0900

    init

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,35 @@
+import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.chrome.ChromeOptions
+import java.util.logging.Level
+import org.openqa.selenium.remote.CapabilityType
+import org.openqa.selenium.logging.LogType
+import org.openqa.selenium.logging.LoggingPreferences
+import org.openqa.selenium.remote.DesiredCapabilities
+
+
+fun main(args: Array<String>) {
+    val capabilities = DesiredCapabilities.chrome()
+    val logPreference = LoggingPreferences()
+    val options = ChromeOptions()
+
+    // ログレベルを設定
+    logPreference.enable(LogType.BROWSER, Level.ALL)
+    capabilities.setCapability(CapabilityType.LOGGING_PREFS, logPreference)
+
+    // ヘッドレスモード
+    options.addArguments("--headless")
+    capabilities.setCapability(ChromeOptions.CAPABILITY, options)
+
+    val driver = ChromeDriver(capabilities)
+
+    driver.get("https://********")
+
+    driver.executeScript("console.log('hoge');")
+    val logs = driver.manage().logs().get("browser")
+
+    logs.forEach { logEntry ->
+        println(logEntry)
+    }
+
+    driver.quit()
+}
